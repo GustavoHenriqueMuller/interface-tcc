2cfdeeecc8d0dbe197fca1e7c0ecb43a6ba70b38
Gustavo Henrique Müller <gustavo.henrique2015@yahoo.com.br>
2023-10-13 13:10:10 -0300

    Update read_request_injector.vhd

--- hdl/slave/test/read_request_injector.vhd
+++ hdl/slave/test/read_request_injector.vhd
@@ -25,7 +25,7 @@ architecture rtl of read_request_injector is
     signal id_w: integer range 0 to 4 := 0;
     signal header_dest_w : std_logic_vector(data_width_p downto 0) := "1" & "0000000000000000" & "0000000000000000";
     signal header_src_w : std_logic_vector(data_width_p downto 0) := "0" & "0000000000000001" & "0000000000000000";
-    signal header_interface_w : std_logic_vector(data_width_p downto 0) := "0" & "0000000000000001" & "0000000000000010";
+    signal header_interface_w : std_logic_vector(data_width_p downto 0) := "0" & "0000000000000001" & "0000000100000010";
     signal address_w : std_logic_vector(data_width_p downto 0) := "0" & "1101110111011101" & "1101110111011101";
     signal trailer_w : std_logic_vector(data_width_p downto 0) := "1" & "1010101010101010" & "1010101010101010";
     signal data_out_w: std_logic_vector(data_width_p downto 0);
