bed9d54a4c40dcfc6e92df6a602cb394e96b4093
Gustavo Henrique Müller <gustavo.henrique2015@yahoo.com.br>
2023-11-01 19:21:54 -0300

    Changed interface flit signals in master front-end

--- hdl/master/frontend/frontend_master.vhd
+++ hdl/master/frontend/frontend_master.vhd
@@ -28,7 +28,7 @@ entity frontend_master is
             -- Write response signals.
             BVALID : out std_logic;
             BREADY : in std_logic;
-            BID    : out std_logic_vector(c_AXI_ID_WIDTH - 1 downto 0) := (others => '0');
+            BID    : out std_logic_vector(c_AXI_ID_WIDTH - 1 downto 0);
             BRESP  : out std_logic_vector(c_AXI_RESP_WIDTH - 1 downto 0);
 
             -- Read request signals.
@@ -45,33 +45,35 @@ entity frontend_master is
             RREADY : in std_logic;
             RDATA  : out std_logic_vector(c_AXI_DATA_WIDTH - 1 downto 0);
             RLAST  : out std_logic;
-            RID    : out std_logic_vector(c_AXI_ID_WIDTH - 1 downto 0) := (others => '0');
+            RID    : out std_logic_vector(c_AXI_ID_WIDTH - 1 downto 0);
             RRESP  : out std_logic_vector(c_AXI_RESP_WIDTH - 1 downto 0);
 
             -- Extra signals.
             CORRUPT_PACKET: out std_logic;
 
         -- Backend signals (injection).
+        i_READY_SEND_DATA  : in std_logic;
+        i_READY_SEND_PACKET: in std_logic;
+
         o_START_SEND_PACKET: out std_logic;
         o_VALID_SEND_DATA  : out std_logic;
         o_LAST_SEND_DATA   : out std_logic;
 
-        i_READY_SEND_DATA  : in std_logic;
-        i_READY_SEND_PACKET: in std_logic;
-
         o_ADDR     : out std_logic_vector(c_AXI_ADDR_WIDTH - 1 downto 0);
-        o_BURST    : out std_logic_vector(1 downto 0);
-        o_LENGTH   : out std_logic_vector(7 downto 0);
-        o_DATA_SEND: out std_logic_vector(c_AXI_DATA_WIDTH - 1 downto 0);
-        o_OPC_SEND : out std_logic;
         o_ID       : out std_logic_vector(c_AXI_ID_WIDTH - 1 downto 0);
+        o_LENGTH   : out std_logic_vector(7 downto 0);
+        o_BURST    : out std_logic_vector(1 downto 0);
+        o_OPC_SEND : out std_logic;
+        o_DATA_SEND: out std_logic_vector(c_AXI_DATA_WIDTH - 1 downto 0);
 
         -- Backend signals (reception).
         i_VALID_RECEIVE_DATA: in std_logic;
         i_LAST_RECEIVE_DATA : in std_logic;
 
-        i_DATA_RECEIVE: in std_logic_vector(c_AXI_DATA_WIDTH - 1 downto 0);
-        i_H_INTERFACE_RECEIVE: in std_logic_vector(c_FLIT_WIDTH - 1 downto 0);
+        i_ID_RECEIVE    : in std_logic_vector(c_AXI_ID_WIDTH - 1 downto 0);
+        i_STATUS_RECEIVE: in std_logic_vector(c_AXI_RESP_WIDTH - 1 downto 0);
+        i_OPC_RECEIVE   : in std_logic;
+        i_DATA_RECEIVE  : in std_logic_vector(c_AXI_DATA_WIDTH - 1 downto 0);
 
         i_CORRUPT_RECEIVE: in std_logic;
 
@@ -84,11 +86,6 @@ architecture rtl of frontend_master is
     -- Injection.
     signal w_OPC_SEND: std_logic;
     signal w_OPC_SEND_OUT: std_logic;
-
-    -- Reception.
-    signal w_OPC_RECEIVE: std_logic;
-    signal w_STATUS_RECEIVE: std_logic_vector(2 downto 0);
-    signal w_ID_RECEIVE: std_logic_vector(c_AXI_ID_WIDTH - 1 downto 0);
 
 begin
     ---------------------------------------------------------------------------------------------
@@ -125,24 +122,20 @@ begin
     ---------------------------------------------------------------------------------------------
     -- Reception.
 
-    w_OPC_RECEIVE    <= i_H_INTERFACE_RECEIVE(1);
-    w_STATUS_RECEIVE <= i_H_INTERFACE_RECEIVE(4 downto 2);
-    w_ID_RECEIVE     <= i_H_INTERFACE_RECEIVE(19 downto 15);
-
-    o_READY_RECEIVE_PACKET <= '1' when (BREADY = '1' and w_OPC_RECEIVE = '0') or
-                                       (RREADY = '1' and w_OPC_RECEIVE = '1') else '0';
+    o_READY_RECEIVE_PACKET <= '1' when (BREADY = '1' and i_OPC_RECEIVE = '0') or
+                                       (RREADY = '1' and i_OPC_RECEIVE = '1') else '0';
 
     o_READY_RECEIVE_DATA   <= RREADY;
 
-    BVALID <= '1' when (i_VALID_RECEIVE_DATA = '1' and w_OPC_RECEIVE = '0') else '0';
-    BID    <= w_ID_RECEIVE when (i_VALID_RECEIVE_DATA = '1' and w_OPC_RECEIVE = '0') else (c_AXI_ID_WIDTH - 1 downto 0 => '0');
-    BRESP  <= w_STATUS_RECEIVE when (i_VALID_RECEIVE_DATA = '1') else (c_AXI_RESP_WIDTH - 1 downto 0 => '0');
+    BVALID <= '1' when (i_VALID_RECEIVE_DATA = '1' and i_OPC_RECEIVE = '0') else '0';
+    BID    <= i_ID_RECEIVE when (i_VALID_RECEIVE_DATA = '1' and i_OPC_RECEIVE = '0') else (c_AXI_ID_WIDTH - 1 downto 0 => '0');
+    BRESP  <= i_STATUS_RECEIVE when (i_VALID_RECEIVE_DATA = '1') else (c_AXI_RESP_WIDTH - 1 downto 0 => '0');
 
-    RVALID <= '1' when (i_VALID_RECEIVE_DATA = '1' and w_OPC_RECEIVE = '1') else '0';
+    RVALID <= '1' when (i_VALID_RECEIVE_DATA = '1' and i_OPC_RECEIVE = '1') else '0';
     RDATA  <= i_DATA_RECEIVE when (i_VALID_RECEIVE_DATA = '1') else (c_AXI_DATA_WIDTH - 1 downto 0 => '0');
     RLAST  <= i_LAST_RECEIVE_DATA;
-    RID    <= w_ID_RECEIVE when (i_VALID_RECEIVE_DATA = '1' and w_OPC_RECEIVE = '1') else (c_AXI_ID_WIDTH - 1 downto 0 => '0');
-    RRESP  <= w_STATUS_RECEIVE when (i_VALID_RECEIVE_DATA = '1') else (c_AXI_RESP_WIDTH - 1 downto 0 => '0');
+    RID    <= i_ID_RECEIVE when (i_VALID_RECEIVE_DATA = '1' and i_OPC_RECEIVE = '1') else (c_AXI_ID_WIDTH - 1 downto 0 => '0');
+    RRESP  <= i_STATUS_RECEIVE when (i_VALID_RECEIVE_DATA = '1') else (c_AXI_RESP_WIDTH - 1 downto 0 => '0');
 
     CORRUPT_PACKET <= i_CORRUPT_RECEIVE;
 end rtl;
