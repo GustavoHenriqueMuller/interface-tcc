a2ab53ec28f2faf576f3da27ae5e950f4af0122a
Gustavo Henrique Müller <gustavo.henrique2015@yahoo.com.br>
2023-11-09 08:27:00 -0300

    Changed c_AXI_ADDR_WIDTH value

--- hdl/tcc_package.vhd
+++ hdl/tcc_package.vhd
@@ -5,9 +5,9 @@ use IEEE.std_logic_1164.all;
 
 package tcc_package is
 	-- AMBA AXI attributes.
-	constant c_AXI_ID_WIDTH  : natural := 5;  -- This constant corresponds to "ID_W_WIDTH" and "ID_R_WIDTH".
-	constant c_AXI_ADDR_WIDTH: natural := 64; -- First half: Operation address. Second half: IP address (XXYY).
 	constant c_AXI_DATA_WIDTH: natural := 32;
+	constant c_AXI_ID_WIDTH  : natural := 5;  -- This constant corresponds to "ID_W_WIDTH" and "ID_R_WIDTH".
+	constant c_AXI_ADDR_WIDTH: natural := c_AXI_DATA_WIDTH * 2; -- First half: Operation address. Second half: IP address (XXYY).
 	constant c_AXI_RESP_WIDTH: natural := 3;  -- This constant corresponds to "BRESP_WIDTH" and "RRESP_WIDTH".
 
 	-- Interface attributes.
